82e73e9fedb529f79d823e5b9ea13119a1de174b
Matthieu Perrelle <matthieu.perrelle@gmail.com>
2026-06-16 16:18:58 +0200

    Building the logic.

--- csp.py
+++ csp.py
@@ -1,8 +1,16 @@
 my_string = "SEND + MORE = MONEY"
 my_letters = []
+first_letter = []
 
 for letter in my_string:
     if letter not in my_letters and letter.isalpha():
         my_letters.append(letter)
 
+first_letter.append(my_letters[0])
+index = my_string.find('+') + 2
+
+if index != -1:
+    first_letter.append(my_string[index])
+
 print(my_letters)
+print(first_letter)
